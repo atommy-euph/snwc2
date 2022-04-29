c00292ed5967d2bcab0432053871195e708c01ac
arata <blacktommy314@gmail.com>
2022-04-29 16:47:08 +0900

    complete prototype

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 import { useState } from "react";
 import "./App.css";
-import { STATIONS } from "./constant/station";
+import {
+  isIncludedInStations,
+  isAnswered,
+  endsWithValidLetter,
+  startsWithValidLetter,
+  getSameGroup,
+} from "./lib/functions";
 
 function App() {
+  const firstStation = "シンカシワ";
+
   const [isGameStart, setIsGameStart] = useState(false);
   const [isGameOver, setIsGameOver] = useState(false);
   const [answer, setAnswer] = useState("");
-  const [answers, setAnswers] = useState(["ヨドヤバシ"]);
+  const [answers, setAnswers] = useState([firstStation]);
 
   const handleAnswer = () => {
+    if (!answer) {
+      setAnswer("");
+      alert("駅名を入力してください");
+      return;
+    }
+    if (!isIncludedInStations(answer)) {
+      setAnswer("");
+      alert("その駅は存在しません");
+      return;
+    }
+    if (!startsWithValidLetter(answer, answers)) {
+      setAnswer("");
+      alert(
+        `「${getSameGroup(
+          lastAnswer.slice(-1)
+        )}」から始まる駅名を入力してください`
+      );
+    }
+    if (isAnswered(answer, answers)) {
+      setIsGameOver(true);
+      return;
+    }
+    if (!endsWithValidLetter(answer, answers)) {
+      setIsGameOver(true);
+      return;
+    }
+
     setAnswers(answers.concat(answer));
     setAnswer("");
     console.log(answers);
+  };
+
+  const restartGame = () => {
+    setAnswers([firstStation]);
+    setAnswer("");
+    setIsGameStart(true);
+    setIsGameOver(false);
   };
 
   const lastAnswer = answers[answers.length - 1];
@@ -53,15 +95,27 @@ function App() {
           </div>
           <div>前の回答: {lastAnswer}</div>
           <div className="mt-10 bg-slate-100">
-            <p className="font-bold mb-2">Previous Answers</p>
-            <ul>
+            <p className="font-bold mb-2">Previous Answer</p>
+            {lastAnswer}
+            {/* <ul>
               {answers.map((value) => (
                 <li key={value} className="text-black">
                   {value}
                 </li>
               ))}
-            </ul>
+            </ul> */}
           </div>
+        </>
+      )}
+      {isGameOver && (
+        <>
+          <div>
+            <p className="text-red">Game Over</p>
+            <p>あなたの回答数: {answers.length}</p>
+          </div>
+          <button className="border-2" onClick={restartGame}>
+            リスタート
+          </button>
         </>
       )}
     </div>
--- src/lib/functions.js
+++ src/lib/functions.js
@@ -1,22 +1,57 @@
 import { STATIONS } from "../constant/station";
+import { GROUPS } from "../constant/groups";
 
-export const isIncludedInData = (answer) => {
+export const isIncludedInStations = (answer) => {
   return STATIONS[answer.length].includes(answer);
+};
+
+export const startsWithValidLetter = (answer, answers) => {
+  return isSameGroup(answers[answers.length - 1].slice(-1), answer.slice(0, 1));
 };
 
 export const isAnswered = (answer, answers) => {
   return answers.includes(answer);
 };
 
-export const endsWithValidInitialLetter = (answer) => {
-  const initialLetters = Array([]);
+export const getCandidates = (answer, answers) => {
+  const candidates = Array([]);
+  const lastLetter = answer.slice(-1);
   for (let key in STATIONS) {
     STATIONS[key].forEach((value) => {
-      const initialLetter = value.slice(0, 1);
-      if (!initialLetters.includes(initialLetter)) {
-        initialLetters.push(initialLetter);
+      if (lastLetter === value.slice(0, 1) && !answers.includes(value)) {
+        candidates.push(value);
       }
     });
   }
-  return initialLetters.includes(answer.slice(-1));
+  return candidates;
+};
+
+export const endsWithValidLetter = (answer, answers) => {
+  const lastLetter = answer.slice(-1);
+
+  const candidates = getCandidates(answer, answers);
+
+  if (lastLetter === "ン") {
+    return false;
+  }
+
+  return candidates.length !== 0 ? true : false;
+};
+
+export const getSameGroup = (a) => {
+  for (let i = 0; i < GROUPS.length; i++) {
+    if (GROUPS[i].includes(a)) {
+      return GROUPS[i];
+    }
+  }
+  return false;
+};
+
+const isSameGroup = (a, b) => {
+  for (let i = 0; i < GROUPS.length; i++) {
+    if (GROUPS[i].includes(a) && GROUPS[i].includes(b)) {
+      return true;
+    }
+  }
+  return false;
 };
