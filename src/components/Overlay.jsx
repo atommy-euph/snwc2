859bcd8a0e568d934c11a3f3fe372f3d178f75f7
arata <blacktommy314@gmail.com>
2022-05-01 14:21:16 +0900

    fix layout of overlay

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -5,7 +5,7 @@ import close from "../icons/close.svg";
 const Overlay = ({ children, title, onClose }) => {
   return (
     <div className="fixed top-0 pt-14 px-6 bg-white w-screen h-screen">
-      <h2 className="fixed top-6 left-6 m-0 text-2xl">{title}</h2>
+      <h2 className="fixed top-6 m-0 text-2xl">{title}</h2>
       <button className="fixed top-4 right-4" onClick={onClose}>
         <img src={close} alt="close" />
       </button>
