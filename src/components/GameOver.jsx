4c6d9aa8eeae6356a9405d84595ce37f07ca9fd0
arata <blacktommy314@gmail.com>
2022-05-03 17:30:19 +0900

    add url to list of answer at gameover

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -11,6 +11,7 @@ import copied from "../icons/copied.svg";
 import { getCandidates, randomSelect } from "../lib/functions";
 
 import { ALERT_TIME, URL } from "../constant/config";
+import { STATION_DATA } from "../constant/station_data";
 
 const GameOver = ({ answers, restartGame, resetGame }) => {
   const [isListOpen, setIsListOpen] = useState(false);
@@ -49,31 +50,52 @@ const GameOver = ({ answers, restartGame, resetGame }) => {
       {isListOpen && (
         <Overlay title="通過駅一覧" onClose={onClose}>
           <div className="mt-8 h-[21rem] border-y-2 py-2 overflow-y-scroll hidden-scrollbar">
-            <p className="font-bold my-0 px-1">
-              (<span className="inline-block w-10 text-center">発</span>)
-              <span className="inline-block w-2"></span>
-              {answers[0].answer}
-            </p>
             {answers
               .map((value) => value.answer)
               .map((value, i) => (
-                <p key={i} className="font-bold my-0 px-1">
-                  (
-                  <span key={i} className="inline-block w-10 text-center">
-                    {i}
+                <div key={i} className="flex flex-row font-bold my-0.5 px-1">
+                  <span key={i} className="inline-block w-8 text-center">
+                    {i === 0 ? "発" : i === answers.length - 1 ? "着" : i}
                   </span>
-                  )<span className="inline-block w-2"></span>
-                  {value}
-                </p>
-              ))
-              .slice(1)}
+                  |<span className="inline-block w-2"></span>
+                  <div className="w-full">
+                    {value}
+                    <span className="text-black font-normal text-sm ml-2">
+                      (
+                      {STATION_DATA[value].map((station) => (
+                        <a
+                          key={station.title}
+                          className="font-normal italic text-sm mx-0.5"
+                          href={station.url}
+                        >
+                          {station.title},
+                        </a>
+                      ))}
+                      )
+                    </span>
+                  </div>
+                </div>
+              ))}
           </div>
           <h3 className="mt-12">続きには...</h3>
-          {nextHints.length > 0 ? (
-            nextHints.map((value) => <span key={value}>{value}, </span>)
-          ) : (
-            <span>回答可能な候補はありませんでした。</span>
-          )}
+          <ul>
+            {nextHints.length > 0 ? (
+              nextHints.map((value) => (
+                <li className="mr-2 my-0.5" key={value}>
+                  {value}{" "}
+                  <a
+                    className="italic text-sm"
+                    href={STATION_DATA[value][0].url}
+                  >
+                    ({STATION_DATA[value][0].title})
+                  </a>
+                  ,
+                </li>
+              ))
+            ) : (
+              <li>回答可能な候補はありませんでした。</li>
+            )}{" "}
+          </ul>
         </Overlay>
       )}
       <Alert
