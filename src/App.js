60ed90af2d84dd251f3d8a9ea616c2970f9673f5
arata <blacktommy314@gmail.com>
2022-05-03 22:09:56 +0900

    make AnsweredList component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { useEffect, useState, useRef, useCallback } from "react";
 
 import Alert from "./components/Alert";
 import Button from "./components/Button.jsx";
-import Overlay from "./components/Overlay";
 import Help from "./components/Help";
 import Info from "./components/Info";
+import AnsweredList from "./components/AnsweredList";
 
 import logo from "./icons/logo_type.svg";
 import frame from "./icons/frame.svg";
@@ -23,8 +23,6 @@ import {
   getSameGroup,
   getFirstStation,
   getLastLetter,
-  getCandidates,
-  randomSelect,
 } from "./lib/functions";
 
 import {
@@ -33,7 +31,6 @@ import {
   COUNTDOWN_TIME,
   GAME_URL,
 } from "./constant/config.js";
-import { STATION_DATA } from "./constant/station_data.js";
 
 function App() {
   // For controling screens
@@ -227,10 +224,6 @@ function App() {
     .reduce((sum, element) => {
       return sum + element;
     }, 0);
-  const candidatesForLastAnswer = randomSelect(
-    getCandidates(answers.slice(-1)[0].answer, answers),
-    4
-  );
 
   const shareResult = () => {
     const text = [
@@ -381,58 +374,10 @@ function App() {
       {isGameOver && (
         <>
           {isListOpen && (
-            <Overlay title="通過駅一覧" onClose={() => setIsListOpen(false)}>
-              <div className="mt-8 h-[21rem] border-y-2 py-2 overflow-y-scroll hidden-scrollbar">
-                {answers
-                  .map((value) => value.answer)
-                  .map((value, i) => (
-                    <div
-                      key={i}
-                      className="flex flex-row font-bold my-0.5 px-1"
-                    >
-                      <span key={i} className="inline-block w-8 text-center">
-                        {i === 0 ? "発" : i === answers.length - 1 ? "着" : i}
-                      </span>
-                      |<span className="inline-block w-2"></span>
-                      <div className="w-full">
-                        {value}
-                        <span className="text-black font-normal text-sm ml-2">
-                          (
-                          {STATION_DATA[value].map((station) => (
-                            <a
-                              key={station.title}
-                              className="font-normal italic text-sm mx-0.5"
-                              href={station.url}
-                            >
-                              {station.title},
-                            </a>
-                          ))}
-                          )
-                        </span>
-                      </div>
-                    </div>
-                  ))}
-              </div>
-              <h3 className="mt-12">続きには...</h3>
-              <ul>
-                {candidatesForLastAnswer.length > 0 ? (
-                  candidatesForLastAnswer.map((value) => (
-                    <li className="mr-2 my-0.5" key={value}>
-                      {value}{" "}
-                      <a
-                        className="italic text-sm"
-                        href={STATION_DATA[value][0].url}
-                      >
-                        ({STATION_DATA[value][0].title})
-                      </a>
-                      ,
-                    </li>
-                  ))
-                ) : (
-                  <li>回答可能な候補はありませんでした。</li>
-                )}{" "}
-              </ul>
-            </Overlay>
+            <AnsweredList
+              answers={answers}
+              onClose={() => setIsListOpen(false)}
+            />
           )}
           <Alert
             message="クリップボードにコピーしました"
