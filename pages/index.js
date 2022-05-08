5a9855320d4f8cf61fbe3f236069bcea131bef3d
arata <blacktommy314@gmail.com>
2022-05-08 18:11:54 +0900

    netlify setting

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import titleLogo from "../public/icons/logo.svg";
 import info from "../public/icons/info.svg";
-import Link from "next/link";
 
 export default function Home() {
   return (
