0571ab8103bae07c6a0f806de55421de4e9f85db
Arata TOYAMA <info@atommy.me>
2023-12-28 20:35:45 +0900

    remove src folder

--- src/constant/config.js
+++
@@ -1,6 +0,0 @@
-export const MINIMUM_INITIAL_NUMBER_OF_CANDIDATES = 10;
-export const ALERT_TIME = 2000; // ms
-export const TIME_LIMIT = 30; // s
-export const MISTAKE_COUNT_LIMIT = 3;
-export const COUNTDOWN_TIME = 3; // s
-export const GAME_URL = "shiritetsu.railword.com";
