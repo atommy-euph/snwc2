1dbd614adb00e404ac18d9b3d7effa4078d9a928
Arata TOYAMA <info@atommy.me>
2023-12-29 11:07:59 +0900

    adjusted layout of ranking screen

--- components/RankingSpeed.jsx
+++ components/RankingSpeed.jsx
@@ -17,12 +17,12 @@ const RankingSpeed = () => {
   return (
     <div className="mt-4 h-[21rem] border-t-2 py-2 overflow-y-scroll hidden-scrollbar">
       <h2 className="mt-4 mb-2">スピード</h2>
-      <div className="flex items-center mb-1">
+      <div className="flex items-center mb-1 text-sm">
         <span className="font-bold text-lg  mr-5  w-8 h-8 "></span>
-        <span className="w-10 mr-3 text-right font-bold"> 記録</span>
-        <span className="w-14 mr-3 text-right font-bold">文字数</span>
-        <span className="w-24 mr-3 text-right font-bold">日付</span>
-        <span className="w-24 mr-3 text-right font-bold">出発駅</span>
+        <span className="w-10 mr-2 text-right font-bold"> 記録</span>
+        <span className="w-14 mr-2 text-right font-bold">文字数</span>
+        <span className="w-24 mr-2 text-right font-bold">日付</span>
+        <span className="w-24 mr-2 text-right font-bold">出発駅</span>
         <span className="w-24 text-right font-bold">終着駅</span>
       </div>
       {recordsSpeed
@@ -33,16 +33,16 @@ const RankingSpeed = () => {
           return b.count - a.count;
         })
         .map((record, index) => (
-          <div className="flex items-center mb-3" key={index}>
+          <div className="flex items-center mb-3 text-sm" key={index}>
             <span className="font-bold text-lg mr-5 border-2 border-black w-8 h-8 text-center">
               {index + 1}
             </span>
-            <span className="w-10 mr-3 text-right"> {record.count} 駅</span>
-            <span className="w-14 mr-3 text-right">{record.letter} 字</span>
-            <span className="w-24 mr-3 text-right">
+            <span className="w-10 mr-2 text-right">{record.count} 駅</span>
+            <span className="w-14 mr-2 text-right">{record.letter} 字</span>
+            <span className="w-24 mr-2 text-right">
               {record.date.slice(0, 10).replaceAll("-", "/")}
             </span>
-            <span className="inline-block whitespace-nowrap w-24 mr-3 text-right overflow-x-scroll hidden-scrollbar">
+            <span className="inline-block whitespace-nowrap w-24 mr-2 text-right overflow-x-scroll hidden-scrollbar">
               {record.start}
             </span>
             <span className="inline-block whitespace-nowrap w-24 text-right overflow-x-scroll hidden-scrollbar">
