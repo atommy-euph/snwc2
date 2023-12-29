73e95ca592b51e67456b853c3ce6180fc0187968
Arata TOYAMA <info@atommy.me>
2023-12-29 11:12:33 +0900

    add whitespace-nowrap

--- components/RankingEndless.jsx
+++ components/RankingEndless.jsx
@@ -19,11 +19,22 @@ const RankingEndless = () => {
       <h2 className="mt-4 mb-2">エンドレス</h2>
       <div className="flex items-center mb-1 text-sm">
         <span className="font-bold text-lg  mr-5  w-8 h-8 "></span>
-        <span className="w-10 mr-2 text-right font-bold"> 記録</span>
-        <span className="w-14 mr-2 text-right font-bold">タイム</span>
-        <span className="w-24 mr-2 text-right font-bold">日付</span>
-        <span className="w-24 mr-2 text-right font-bold">出発駅</span>
-        <span className="w-24 text-right font-bold">終着駅</span>
+        <span className="w-10 mr-2 text-right whitespace-nowrap  font-bold">
+          {" "}
+          記録
+        </span>
+        <span className="w-14 mr-2 text-right whitespace-nowrap  font-bold">
+          タイム
+        </span>
+        <span className="w-24 mr-2 text-right whitespace-nowrap  font-bold">
+          日付
+        </span>
+        <span className="w-24 mr-2 text-right whitespace-nowrap  font-bold">
+          出発駅
+        </span>
+        <span className="w-24 text-right whitespace-nowrap  font-bold">
+          終着駅
+        </span>
       </div>
       {recordsEndless
         .sort((a, b) => {
@@ -37,9 +48,14 @@ const RankingEndless = () => {
             <span className="font-bold text-lg mr-5 border-2 border-black w-8 h-8 text-center">
               {index + 1}
             </span>
-            <span className="w-10 mr-2 text-right"> {record.count} 駅</span>
-            <span className="w-14 mr-2 text-right">{record.time} 秒</span>
-            <span className="w-24 mr-2 text-right">
+            <span className="w-10 mr-2 whitespace-nowrap  text-right">
+              {" "}
+              {record.count} 駅
+            </span>
+            <span className="w-14 mr-2 whitespace-nowrap  text-right">
+              {record.time} 秒
+            </span>
+            <span className="w-24 mr-2 whitespace-nowrap  text-right">
               {record.date.slice(0, 10).replaceAll("-", "/")}
             </span>
             <span className="inline-block whitespace-nowrap w-24 mr-2 text-right overflow-x-scroll hidden-scrollbar">
