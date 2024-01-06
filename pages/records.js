c638902fbe26058ecd3af13cde399a94eede2c0f
Arata TOYAMA <info@atommy.me>
2024-01-06 09:22:56 +0900

    bold switch in ranking page

--- pages/records.js
+++ pages/records.js
@@ -12,7 +12,7 @@ export default function Records() {
     <Page title="ランキング">
       <div className="flex justify-end mt-5">
         <button
-          className={`px-2 py-1 text-sm ${
+          className={`px-2 py-1 text-sm font-bold ${
             which === "endless"
               ? "bg-gray-300 border-2 border-black"
               : "bg-gray-200"
@@ -22,7 +22,7 @@ export default function Records() {
           エンドレス
         </button>
         <button
-          className={`px-2 py-1 text-sm ${
+          className={`px-2 py-1 text-sm font-bold ${
             which === "speed"
               ? "bg-gray-300 border-2 border-black"
               : "bg-gray-200"
